1fbd9265fa0c8c145feb2d7569a70149d3c7702b
RPLaine <riku.laine@gmail.com>
2023-12-04 13:08:05 +0200

    Update views.py
    Fixed index method and started a route method

--- demoproject/discordlogin/views.py
+++ demoproject/discordlogin/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, JsonResponse
 
-def index(request: HttpRequest) -> HttpResponse:
+def index(request: HttpRequest) -> JsonResponse:
     return JsonResponse({'message': 'Hello, world!'})
+
+def discord_login(request: HttpRequest) -> HttpResponse:
+    return render(request, 'discordlogin/discord-login.html')
