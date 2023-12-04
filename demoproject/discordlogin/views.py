951b3705885e4e8004605f2e17da4c5b26537837
RPLaine <riku.laine@gmail.com>
2023-12-04 13:06:12 +0200

    URL finds path to JSON

--- demoproject/discordlogin/views.py
+++ demoproject/discordlogin/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, JsonResponse
 
-# Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    return JsonResponse({'message': 'Hello, world!'})
