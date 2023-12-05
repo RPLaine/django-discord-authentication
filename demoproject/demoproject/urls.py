7215e61c332a4e7904ac4f4eea9c38a189411233
RPLaine <riku.laine@gmail.com>
2023-12-05 10:49:26 +0200

    Added root redirect to oauth app

--- demoproject/demoproject/urls.py
+++ demoproject/demoproject/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect("oauth/")),
     path('admin/', admin.site.urls),
     path('oauth/', include('discordlogin.urls')),
 ]
