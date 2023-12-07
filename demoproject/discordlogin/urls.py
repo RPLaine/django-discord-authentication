537381c446667cdb9a02d932e954d5731b03ccb7
RPLaine <riku.laine@gmail.com>
2023-12-07 14:39:41 +0200

    Added a method to get user data
    Also placed Django redirects so that the authorization works from the root

--- demoproject/discordlogin/urls.py
+++ demoproject/discordlogin/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', lambda req: redirect("login/")),
     path('login/', views.discord_login, name='discord_login'),
     path('login/redirect', views.discord_login_redirect, name='discord_login_redirect')
 ]
--- demoproject/discordlogin/views.py
+++ demoproject/discordlogin/views.py
@@ -1,14 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import redirect
+import requests
 
 auth_url_discord = "https://discord.com/api/oauth2/authorize?client_id=1181190931827916852&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Foauth2%2Flogin%2Fredirect&scope=identify"
-
-def index(request: HttpRequest) -> JsonResponse:
-    return JsonResponse({'message': 'Hello, world!'})
 
 def discord_login(request: HttpRequest):
     return redirect(auth_url_discord)
 
 def discord_login_redirect(request: HttpRequest):
-    return JsonResponse({'message': 'Redirected!'})
+    if request.GET.get('code'):
+        code = request.GET.get('code')
+    else:
+        code = "No code"
+
+    user = exchange_code(code)
+
+    return JsonResponse({
+        'user': user,
+        'code': code
+        })
+
+client_id = '1181190931827916852'
+client_secret = 'cAb_hMZHAzu5WTv2OlIMIk9LHHeVZaLg'
+
+def exchange_code(code: str):
+    data = {
+        'client_id': client_id, #This should be stored in an environment variable in production
+        'client_secret': client_secret, #This should be stored in an environment variable in production
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': 'http://localhost:8000/oauth2/login/redirect', #In production, this should be changed to the actual domain
+        'scope': 'identify'
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    response = requests.post('https://discord.com/api/oauth2/token', data=data, headers=headers) # the Token endpoint of Discord's OAuth2 provider
+    credentials = response.json()
+    access_token = credentials['access_token']
+    response = requests.get('https://discord.com/api/users/@me', headers={'Authorization': f'Bearer {access_token}'})
+    user = response.json()
+    return user
