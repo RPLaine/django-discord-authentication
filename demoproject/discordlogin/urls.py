b6cb1a9b425967b09927b5e19da9573b8a4c0237
RPLaine <riku.laine@gmail.com>
2023-12-07 13:51:13 +0200

    Redirect from Discord
    Discord redirects back to Django. Next step: read the authentication data.

--- demoproject/discordlogin/urls.py
+++ demoproject/discordlogin/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.discord_login, name='discord_login'),
+    path('login/redirect', views.discord_login_redirect, name='discord_login_redirect')
 ]
--- demoproject/discordlogin/views.py
+++ demoproject/discordlogin/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import redirect
 
-auth_url_discord = "https://discord.com/api/oauth2/authorize?client_id=1181190931827916852&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Foauth%2Flogin%2Fredirect&scope=identify"
+auth_url_discord = "https://discord.com/api/oauth2/authorize?client_id=1181190931827916852&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Foauth2%2Flogin%2Fredirect&scope=identify"
 
 def index(request: HttpRequest) -> JsonResponse:
     return JsonResponse({'message': 'Hello, world!'})
 
 def discord_login(request: HttpRequest):
     return redirect(auth_url_discord)
+
+def discord_login_redirect(request: HttpRequest):
+    return JsonResponse({'message': 'Redirected!'})
