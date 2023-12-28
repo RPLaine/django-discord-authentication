2293d0e672dd125b87bce069ed822c2de6aa08a3
RPLaine <riku.laine@gmail.com>
2023-12-28 10:56:24 +0200

    Update models.py
    Created UserData model that matches discord data from discordlogin

--- demoproject/discordlogin/models.py
+++ demoproject/discordlogin/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class UserData(models.Model):
+    id = models.AutoField(primary_key=True)
+    discord_id = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    avatar = models.CharField(max_length=255)
+    discriminator = models.CharField(max_length=255)
+    public_flags = models.IntegerField()
+    premium_type = models.IntegerField()
+    flags = models.IntegerField()
+    banner = models.CharField(max_length=255)
+    accent_color = models.CharField(max_length=255)
+    global_name = models.CharField(max_length=255)
+    avatar_decoration_data = models.CharField(max_length=255)
+    banner_color = models.CharField(max_length=255)
+    mfa_enabled = models.BooleanField()
+    locale = models.CharField(max_length=255)
